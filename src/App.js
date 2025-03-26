ee487c06a3144fedcd163d5e3d08bde57b4863b0
jongwon <justine25@naver.com>
2025-03-26 14:27:24 +0900

    포트폴리오 시작

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>황종원 Portfolio</h1>
+
+      <div className="pf-container">
+        <div className="pf-video">
+          <h3>주제 : ~ </h3>
+          <p>일정 : ~ </p>
+          <video src={"/videos/pf3.mkv"} autoPlay muted loop></video>
+        </div>
+        <div className="pf-video">
+          <video src={"/videos/pf1.mp4"} autoPlay muted loop></video>
+        </div>
+        <div className="pf-video">
+          <video src={"/videos/pf2.mp4"} autoPlay muted loop></video>
+        </div>
+      </div>
     </div>
   );
 }
