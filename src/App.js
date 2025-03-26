cec9f62bf948a6f945ecabdc101bd9b236cec783
jongwon <justine25@naver.com>
2025-03-26 14:53:04 +0900

    프로젝트 3개 영상 업로드

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
       <h1>황종원 Portfolio</h1>
 
       <div className="pf-container">
+  
+        <div className="pf-video">
+          <video src={"https://jongwon0224.github.io/web-portfolio/videos/pf1.mp4"} autoPlay muted loop></video>
+        </div>
+        
+        <div className="pf-video">
+          <video src={"https://jongwon0224.github.io/web-portfolio/videos/pf2.mp4"} autoPlay muted loop></video>
+        </div>
+
         <div className="pf-video">
           <h3>주제 : ~ </h3>
           <p>일정 : ~ </p>
-          <video src={"/videos/pf3.mkv"} autoPlay muted loop></video>
-        </div>
-        <div className="pf-video">
-          <video src={"/videos/pf1.mp4"} autoPlay muted loop></video>
-        </div>
-        <div className="pf-video">
-          <video src={"/videos/pf2.mp4"} autoPlay muted loop></video>
+          <video src={"https://jongwon0224.github.io/web-portfolio/videos/pf3.mp4"} autoPlay muted loop></video>
         </div>
       </div>
     </div>
